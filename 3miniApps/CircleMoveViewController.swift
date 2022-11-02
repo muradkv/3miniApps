7b036361f92c694cb594aab3f977038fbabf9388
murad <muradkv@gmail.com>
2022-11-02 11:44:04 +0300

    Fixed bugs with borders where circle cant move in CircleMove mini app

--- 3miniApps/CircleMoveViewController.swift
+++ 3miniApps/CircleMoveViewController.swift
@@ -7,14 +7,20 @@
 
 import UIKit
 
+enum Direction {
+    case right
+    case left
+    case up
+    case down
+}
+
 class CircleMoveViewController: UIViewController {
     
     @IBOutlet var upButton: UIButton!
     
     let circle = UIView()
-    let circleWidth = 15
-    let circleHeight = 15
-    let stepForCirle = 30
+    let circleSide = 15
+    let stepForCirle = 10
     var position: (x: Int, y: Int) = (0, 0)
 
     override func viewDidLoad() {
@@ -26,38 +32,46 @@ class CircleMoveViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        position.x = (Int(view.frame.width / 2) - circleWidth / 2)
-        position.y = (Int(view.frame.height / 2) - circleHeight / 2)
+        let xCenter = (Int(view.frame.width / 2) - circleSide / 2)
+        let yCenter = (Int(view.frame.height / 2) - circleSide / 2)
         
-        circle.frame = CGRect(x: position.x, y: position.y, width: circleWidth, height: circleHeight)
+        circle.frame = CGRect(x: xCenter, y: yCenter, width: circleSide, height: circleSide)
         circle.backgroundColor = .systemPurple
-        circle.layer.cornerRadius = CGFloat(circleWidth / 2)
+        circle.layer.cornerRadius = CGFloat(circleSide / 2)
         view.addSubview(circle)
     }
     
     @IBAction func upPressed(_ sender: UIButton) {
-        if (position.y - circleHeight) < 0 { return }
-        position.y -= stepForCirle
-        circle.frame.origin.y = CGFloat(position.y)
+        if border(direction: .up) < 0 { return }
+        circle.frame.origin.y -= CGFloat(stepForCirle)
     }
     
     @IBAction func leftPressed(_ sender: UIButton) {
-        if (position.x - circleWidth) < 0 { return }
-        position.x -= stepForCirle
-        circle.frame.origin.x = CGFloat(position.x)
+        if border(direction: .left)  < 0 { return }
+        circle.frame.origin.x -= CGFloat(stepForCirle)
     }
     
     @IBAction func rightPressed(_ sender: UIButton) {
-        if (position.x + circleWidth) > Int(view.frame.width) { return }
-        position.x += stepForCirle
-        circle.frame.origin.x = CGFloat(position.x)
+        if border(direction: .right) > Int(view.frame.width)  { return }
+        circle.frame.origin.x += CGFloat(stepForCirle)
     }
     
     @IBAction func downPressed(_ sender: UIButton) {
-        let bottomLine = upButton.frame.origin.y - 5
-        
-        if (position.y + circleHeight) > Int(bottomLine)  { return }
-        position.y += stepForCirle
-        circle.frame.origin.y = CGFloat(position.y)
+        if border(direction: .down) > Int(upButton.frame.origin.y)  { return }
+        circle.frame.origin.y += CGFloat(stepForCirle)
     }
+    
+    func border(direction: Direction) -> Int {
+        switch direction {
+        case .right:
+            return Int(circle.frame.origin.x) + stepForCirle + circleSide
+        case .left:
+            return Int(circle.frame.origin.x) - stepForCirle - circleSide
+        case .up:
+            return Int(circle.frame.origin.y) - stepForCirle - circleSide
+        case .down:
+            return Int(circle.frame.origin.y) + stepForCirle + circleSide
+        }
+    }
+
 }
